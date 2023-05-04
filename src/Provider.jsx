ed6c2c3f94a388bf6e985a61490eecf802a6afd6
Mohammad Alavi <43592603+mohammad00alavi@users.noreply.github.com>
2023-05-04 22:58:12 +0100

    added edit posts

--- src/Provider.jsx
+++ src/Provider.jsx
@@ -1,6 +1,7 @@
-import { createContext, useCallback, useState } from "react"
+import { createContext } from "react"
 import useFetchPosts from "./hooks/useFetchPosts";
 import useWritePosts from "./hooks/useWritePosts";
+import usePostEdit from "./hooks/usePostEdit";
 
 const BlogContext = createContext({
   blogPosts: [],
@@ -9,11 +10,13 @@ const BlogContext = createContext({
   error: '',
   fetchPosts: () => {},
   writePosts: (post) => {},
+  editPost: (post, id) => {},
 });
 
 export const BlogProvider = ({ children }) => {
   const { blogPosts, isLoading, setIsLoading, error, fetchPosts } = useFetchPosts();
   const writePosts = useWritePosts();
+  const editPost = usePostEdit();
 
   const context = {
     blogPosts,
@@ -22,7 +25,7 @@ export const BlogProvider = ({ children }) => {
     error,
     fetchPosts,
     writePosts,
-
+    editPost,
   }
   return (
     <BlogContext.Provider value={context} >
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import usePosts from "../hooks/usePosts";
 import Post from "./Post";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const { blogPosts, error, isLoading, fetchPosts } = usePosts();
+  const { blogPosts, isLoading, fetchPosts } = usePosts();
   useEffect(() => {
     fetchPosts();
   },[])
@@ -18,8 +19,10 @@ export default function Home() {
     <>
       <h1>Home</h1>
       <div>
-        {blogPosts?.map(item => (
-          <Post key={item.id} title={item.title} description={item.description} postDate={item.date} author={item.author} />
+        {blogPosts?.map((item) => (
+          <Link key={item.id} to={`/blog/${item.id}`}>
+            <Post title={item.title} description={item.description} postDate={item.date} author={item.author} />
+          </Link>
         ))}
       </div>
     </>
